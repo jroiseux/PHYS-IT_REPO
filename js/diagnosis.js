982616d9b2ab84f591ce68df8aa45e9d71b0244e
Jolie Roiseux <jo.roiseux@gmail.com>
2018-11-29 21:39:14 -0800

    convert to list add delete list item

--- js/diagnosis.js
+++ js/diagnosis.js
@@ -8,60 +8,79 @@ patientList.on('value', function(snap) {
 
 });
 
-
-function saveExercise(text) {
-  var newPostKey = firebase.database().ref().child("Patients/" + patientid + "/MedicalInfo/Exercises").push().key;
-  var updates = {};
-  updates["Patients/" + patientid + "/MedicalInfo/Exercises/" + newPostKey] = {'exercise': text};
-  firebase.database().ref().update(updates);
-  $('#Exercises').html('');
-
-};
-
-function displayExercises(textareaid) {
-  //$('#' + textareaid).html("");
-  firebase.database()
-  .ref("Patients/" + patientid + "/MedicalInfo/Exercises/")
-  .on('value', function(snap) {
-    exercises = snap.val();
-    $('#' + textareaid).html("");
-    snap.forEach(function(e){
-      $('#' + textareaid).append(e.val().exercise + '\n');
+function displayList() {
+  var ref = firebase.database().ref("exercises/knee");
+  ref.on(
+    "value",
+    function(snap) {
+      snap.forEach(function(snapshot){
+        var ex = snapshot.val();
+        console.log(ex);
+        $('.dropdown-content').append('<a onclick="addExercise(\''+ex+'\')">'+ex+'</a>');
+      });
     });
-  });
-}
+  }
 
-
-function createPDF(){
-  var doc = new jsPDF()
-  var specialElementHandlers = {
-    '#editor': function (element, renderer) {
-      return true;
-    }
-  };
-  var content = "";
-  patientList.on('value', function(snap) {
-    doc.text(snap.val().FirstName + " " + snap.val().LastName, 10, 15);
-    doc.text("Exercises:", 10, 25);
-    var height = 35;
+  function displayExercises() {
+    $('#exerciseList').html("<ul id='list'>");
     firebase.database()
     .ref("Patients/" + patientid + "/MedicalInfo/Exercises/")
     .once('value', function(snap) {
       exercises = snap.val();
       snap.forEach(function(e){
-        doc.text("  - " + e.val().exercise, 10, height);
-        height = height + 10;
+        $('#list').append('<li id="'+e.key+'">' + e.val().exercise
+        + '<img onclick="removeExercise(\''+e.key+'\'); displayExercises()" class="deleteimage" src="images/delete.svg"></li>');
+      });
+      $('#list').append("</ul>");
+    });
+  }
+
+  function addExercise(text){
+    var newPostKey = firebase.database().ref().child("Patients/" + patientid + "/MedicalInfo/Exercises").push().key;
+    var updates = {};
+    updates["Patients/" + patientid + "/MedicalInfo/Exercises/" + newPostKey] = {'exercise': text};
+    firebase.database().ref().update(updates);
+    $('#Exercises').html('');
+    $('#list').append('<li id="'+newPostKey+'">' + text + '<img class="deleteimage" src="images/delete.svg"onclick="removeExercise(\''+newPostKey+'\'); displayExercises()"></li>');
+  }
+
+  function removeExercise(key){
+    firebase.database()
+    .ref("Patients/" + patientid + "/MedicalInfo/Exercises/")
+    .child(key).remove();
+  }
+
+  function createPDF(){
+    var doc = new jsPDF()
+    var specialElementHandlers = {
+      '#editor': function (element, renderer) {
+        return true;
+      }
+    };
+    var content = "";
+    patientList.on('value', function(snap) {
+      doc.text(snap.val().FirstName + " " + snap.val().LastName, 10, 15);
+      doc.text("Exercises:", 10, 25);
+      var height = 35;
+      firebase.database()
+      .ref("Patients/" + patientid + "/MedicalInfo/Exercises/")
+      .once('value', function(snap) {
+        exercises = snap.val();
+        snap.forEach(function(e){
+          doc.text("  - " + e.val().exercise, 10, height);
+          height = height + 10;
+        });
       });
     });
+    doc.save('exercises.pdf')
+  }
+
+  $(document).ready(function() {
+    displayText('Diagnosis', 'diagnosisTextarea');
+    displayText('Diagnosis', 'Diagnosis');
+    displayList();
+    //displayText('Exercises', 'exercisesTextarea');
+    //displayText('Exercises', 'Exercises');
+    displayExercises();
+
   });
-  doc.save('exercises.pdf')
-}
-
-$(document).ready(function() {
-  displayText('Diagnosis', 'diagnosisTextarea');
-  displayText('Diagnosis', 'Diagnosis');
-  //displayText('Exercises', 'exercisesTextarea');
-  //displayText('Exercises', 'Exercises');
-  displayExercises('exercisesTextarea');
-
-});
